9811702b0445e0aabb16b1445d0b7395c6041c6f
whalebab <juncoder94@gmail.com>
2024-04-29 21:21:01 +0900

    AppConfig를 사용한 관심사의 분리(Dependency Injection)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,8 +10,15 @@ import hello.core.order.OrderServiceImpl;
 // 애플리케이션 전체를 설정하고 구성
 public class AppConfig {
 
+    /**
+     * AppConfig는 생성한 객체 인슨턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다. (생성자 주입)
+     * */
+    // MemberServiceImpl -> MemoryMemberRepository
+    // OrderServiceImpl -> MemoryMemberRepository, FixDiscountPolicy
+
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
+        // MemberServiceImpl를 만들고 내가 만드는 MemberServiceImpl은 MemoryMemberRepository를 사용할거야 (주입해줌)
     }
 
     public OrderService orderService() {
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,26 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    // @BeforeEach 는 각 테스트를 실행하기 전에 호출
+    @BeforeEach
+    public void BeforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
