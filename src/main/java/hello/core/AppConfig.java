a2053f2eb08f596dc876e4f3d4ddc41b1fcac57a
whalebab <juncoder94@gmail.com>
2024-04-29 22:19:33 +0900

    Ioc, DI, Spring Container 개념정리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,27 +1,42 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션 전체를 설정하고 구성
-public class AppConfig {
+@Configuration // 설정 정보
+public class AppConfig { // IOC 컨테이너, DI 컨테이너
 
-    /**
-     * AppConfig는 생성한 객체 인슨턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다. (생성자 주입)
-     * */
-    // MemberServiceImpl -> MemoryMemberRepository
-    // OrderServiceImpl -> MemoryMemberRepository, FixDiscountPolicy
+    // @Configuration : 스프링 컨테이너의 설정(구성)정보
+    // @Bean : 여기서 @Bean이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록함 / @Bean 메서드 명을 스프링의 이름으로 사용함
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
-        // MemberServiceImpl를 만들고 내가 만드는 MemberServiceImpl은 MemoryMemberRepository를 사용할거야 (주입해줌)
+    @Bean
+    public MemberService memberService() { // 역할
+        return new MemberServiceImpl(memberRepository()); // 구현
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    @Bean
+    public OrderService orderService() { // 역할
+        return new OrderServiceImpl(memberRepository(), discountPolicy()); // 구현
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // ApplicationContext : 스프링 컨테이너 (모든 (객체)정보들을 관리해줌 / @Bean으로 선언한 것을 관리해줌)
+        // ※ AppConfig의 설정 정보를 가지고 스프링이 AppConfig 클래스의 @Bean 붙어 있는 정보(객체)들을 Spring Container 등록해서 관리해 줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
